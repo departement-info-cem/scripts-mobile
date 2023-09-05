6da86750a5a879e23bd8f27fb41a92614a767e2c
deguet <joris.deguet@gmail.com>
2023-09-05 14:55:47 -0400

    creation d'un script qui suit le status de l'installation

---
+++ sub-scripts/status.ps1
@@ -0,0 +1,22 @@
+﻿$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
+
+. "$PSScriptRoot\urls-et-versions.ps1"
+. "$PSScriptRoot\fonctions.ps1"
+
+Invoke-Env-Reload
+
+Do {
+
+  $androidstudio = (Test-Path '$HOME\android-studio\bin')
+  $java = Test-CommandExists('java')
+  $flutter = Test-CommandExists('flutter')
+
+  Write-Host 'status des scripts'
+  Write-Host ' android studio'$androidstudio
+  Write-Host ' flutter'$flutter
+  Write-Host ' Java'$java
+  
+  Start-Sleep -s 5  # attend 10 seecondes avant de regarder
+
+} While ($true)
