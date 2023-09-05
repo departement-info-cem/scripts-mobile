9e2613e6c1604b3abfa4936cabf5c8ef57474cd5
deguet <joris.deguet@gmail.com>
2023-09-05 15:25:14 -0400

    cleaner fake start script

--- sub-scripts/start-flutter.ps1
+++ sub-scripts/start-flutter.ps1
@@ -1,1 +1,14 @@
 
+﻿. "$PSScriptRoot\urls-et-versions.ps1"
+. "$PSScriptRoot\fonctions.ps1"
+
+Invoke-Env-Reload
+
+Write-Host '    👍 Création de projet fake pour first run.' -ForegroundColor Blue
+Set-Location $HOME
+flutter create fake_start
+Write-Host '    👍 Premier démarrage.' -ForegroundColor Blue
+Set-Location $HOME\fake_start
+
+#TODO wait on emulator to exist
+flutter run
