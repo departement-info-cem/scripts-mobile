a2ef6a5cac048902446694558692e13cf4ecb429
Joris Deguet <joris.deguet@gmail.com>
2023-09-07 10:47:18 -0400

    Arrete quand tout est beau

--- sub-scripts/status.ps1
+++ sub-scripts/status.ps1
@@ -7,38 +7,44 @@ Write-Host 'Script de status sur les installations :::' -ForegroundColor Blue
 Do {
   Write-Host '---------------------------------------------'
   $date = Get-Date
+  $allgood = $true
   Write-Host "status des installations @ $date" -ForegroundColor Blue
 
   if (-Not ( Test-Path "$HOME\AppData\Local\Android\Sdk\emulator" )) {
     Write-Host '    Android SDK nope.' -ForegroundColor Red
+    $allgood = $false
   }
   else {
     Write-Host '    Android SDK ok.' -ForegroundColor Green
   }
   if (-Not ( Test-Path "$HOME\jdk\fini.txt" )) {
     Write-Host '    Java JDK nope.' -ForegroundColor Red
+    $allgood = $false
   }
   else {
     Write-Host '    Java JDK  ok.' -ForegroundColor Green
   }
   if (-Not ( Test-Path "$HOME\flutter" )) {
     Write-Host '    Flutter SDK nope.' -ForegroundColor Red
+    $allgood = $false
   }
   else {
     Write-Host '    Flutter JDK ok.' -ForegroundColor Green
   }
   if (-Not ( Test-Path "$HOME\android-studio" )) {
     Write-Host '    Android Studio nope.'  -ForegroundColor Red
+    $allgood = $false
   }
   else {
     Write-Host '    Android Studio est déjà installé.'  -ForegroundColor Green
   }
   if (-Not ( Test-Path "$HOME\idea\fini.txt" )) {
     Write-Host '    Intellij nope.'  -ForegroundColor Red
+    $allgood = $false
   }
   else {
     Write-Host '    Intellij ok.'  -ForegroundColor Green
   }
   Start-Sleep -s 10  # attend 10 seecondes avant de regarder
-} While ($true)
+} While ($allgood -eq $false)
 
