f21eff3296e0cc656f33a541f005c1d4bdf31727
deguet <joris.deguet@gmail.com>
2023-09-24 11:46:50 -0400

    Enlevage des plugins de Intellij

--- sub-scripts/ij-installe.ps1
+++ sub-scripts/ij-installe.ps1
@@ -8,8 +8,8 @@ Write-Host "Intellij Idea installation"
 if (-Not ( Test-Path $HOME\idea )) {
 
     Invoke-Install "IntelliJ Idea" "$HOME\idea"  "idea.zip"
-    Invoke-Install "plugin Flutter" "$HOME\idea\plugins" "plugin-flutter-idea.zip"
-    Invoke-Install "plugin Dart" "$HOME\idea\plugins" "plugin-dart-idea.zip"
+    #Invoke-Install "plugin Flutter" "$HOME\idea\plugins" "plugin-flutter-idea.zip"
+    #Invoke-Install "plugin Dart" "$HOME\idea\plugins" "plugin-dart-idea.zip"
     Add-Shortcut $HOME\idea\bin\idea64.exe "IntelliJ IDEA Community"
     Add-Env "Path" "$HOME\idea\bin"
     Start-Process -FilePath "$HOME\idea\bin\idea64.exe"
