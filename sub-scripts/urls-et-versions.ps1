b2bb430d751c1895fb0604eb6e261ea1a2f702d6
deguet <joris.deguet@cegepmontpetit.ca>
2023-10-10 07:31:55 -0400

    Version changes

--- sub-scripts/urls-et-versions.ps1
+++ sub-scripts/urls-et-versions.ps1
@@ -2,7 +2,7 @@
 $SEVENZIP_URL = 'https://www.7-zip.org/a/7z2301-x64.exe'
 # Android Studio
 
-$STUDIO_URL = 'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-windows.zip' # https://developer.android.com/studio
+$STUDIO_URL = 'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.20/android-studio-2022.3.1.20-windows.zip' # https://developer.android.com/studio
 $FLUTTER_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=372030' # https://plugins.jetbrains.com/plugin/9212-flutter/versions
 $DART_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=332609' # https://plugins.jetbrains.com/plugin/6351-dart/versions
 $FLUTTER_INTL_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=374049'
@@ -23,5 +23,5 @@ $IDEA_URL = 'https://download.jetbrains.com/idea/ideaIC-2023.2.2.win.zip' # http
 
 # Flutter
 
-$FLUTTER_SDK = 'https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.13.4-stable.zip' # https://docs.flutter.dev/get-started/install/windows
+$FLUTTER_SDK = 'https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.13.6-stable.zip' # https://docs.flutter.dev/get-started/install/windows
 $NODE_URL = 'https://nodejs.org/dist/v18.16.0/node-v18.16.0-win-x64.zip'
