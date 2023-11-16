1a48cd4b6f47d918aa328a4c04e797d966285393
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2023-11-16 09:43:02 -0500

    Versions

--- sub-scripts/urls-et-versions.ps1
+++ sub-scripts/urls-et-versions.ps1
@@ -2,26 +2,19 @@
 $SEVENZIP_URL = 'https://www.7-zip.org/a/7z2301-x64.exe'
 # Android Studio
 
-$STUDIO_URL = 'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.19/android-studio-2022.3.1.19-windows.zip' # https://developer.android.com/studio
-$FLUTTER_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=372030' # https://plugins.jetbrains.com/plugin/9212-flutter/versions
-$DART_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=332609' # https://plugins.jetbrains.com/plugin/6351-dart/versions
-$FLUTTER_INTL_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=374049'
-
-$CURRENT_SDK_VERSION = '30'
-$CURRENT_BUILD_TOOLS_VERSION = '30.0.2'
+$STUDIO_URL = 'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.21/android-studio-2022.3.1.21-windows.zip' # https://developer.android.com/studio
+$FLUTTER_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=434684' # https://plugins.jetbrains.com/plugin/9212-flutter/versions
+$DART_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=424373' # https://plugins.jetbrains.com/plugin/6351-dart/versions
+$FLUTTER_INTL_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=374050' # https://plugins.jetbrains.com/plugin/13666-flutter-intl/versions
 
 # Android SDK et sdkmanager
 
 $CORRETTO_URL = 'https://corretto.aws/downloads/latest/amazon-corretto-17-x64-windows-jdk.zip'
 
-$CMD_LINE_TOOLS_URL = 'https://dl.google.com/android/repository/commandlinetools-win-10406996_latest.zip' # https://developer.android.com/studio#command-tools
-$ANDROID_PLATFORM_TOOLS = 'https://dl.google.com/android/repository/platform-tools-latest-windows.zip'
-
 # IntelliJ
 
-$IDEA_URL = 'https://download.jetbrains.com/idea/ideaIC-2023.2.2.win.zip' # https://www.jetbrains.com/idea/download/other.html
+$IDEA_URL = 'https://download.jetbrains.com/idea/ideaIC-2023.2.5.win.zip' # https://www.jetbrains.com/idea/download/other.html
 
 # Flutter
 
-$FLUTTER_SDK = 'https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.13.9-stable.zip' # https://docs.flutter.dev/get-started/install/windows
-$NODE_URL = 'https://nodejs.org/dist/v18.16.0/node-v18.16.0-win-x64.zip'
+$FLUTTER_SDK = 'https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.16.0-stable.zip' # https://docs.flutter.dev/get-started/install/windows
