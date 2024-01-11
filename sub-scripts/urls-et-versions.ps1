7accf5ca0cb3924a1d6c2d3024303b51edc775d7
Joris Deguet <joris.deguet@gmail.com>
2024-01-11 11:15:20 -0500

    Version updates pour H24

--- sub-scripts/urls-et-versions.ps1
+++ sub-scripts/urls-et-versions.ps1
@@ -2,9 +2,9 @@
 $SEVENZIP_URL = 'https://www.7-zip.org/a/7z2301-x64.exe'
 # Android Studio
 
-$STUDIO_URL = 'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2022.3.1.21/android-studio-2022.3.1.21-windows.zip' # https://developer.android.com/studio
-$FLUTTER_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=434684' # https://plugins.jetbrains.com/plugin/9212-flutter/versions
-$DART_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=424373' # https://plugins.jetbrains.com/plugin/6351-dart/versions
+$STUDIO_URL = 'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.27/android-studio-2023.1.1.27-windows.zip' # https://developer.android.com/studio
+$FLUTTER_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=453510' # https://plugins.jetbrains.com/plugin/9212-flutter/versions
+$DART_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=464247' # https://plugins.jetbrains.com/plugin/6351-dart/versions
 $FLUTTER_INTL_PLUGIN_URL_STUDIO = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=374050' # https://plugins.jetbrains.com/plugin/13666-flutter-intl/versions
 
 # Android SDK et sdkmanager
@@ -13,8 +13,8 @@ $CORRETTO_URL = 'https://corretto.aws/downloads/latest/amazon-corretto-17-x64-wi
 
 # IntelliJ
 
-$IDEA_URL = 'https://download.jetbrains.com/idea/ideaIC-2023.2.5.win.zip' # https://www.jetbrains.com/idea/download/other.html
+$IDEA_URL = 'https://download.jetbrains.com/idea/ideaIC-2023.3.2.win.zip' # https://www.jetbrains.com/idea/download/other.html
 
 # Flutter
 
-$FLUTTER_SDK = 'https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.16.0-stable.zip' # https://docs.flutter.dev/get-started/install/windows
+$FLUTTER_SDK = 'https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.16.6-stable.zip' # https://docs.flutter.dev/get-started/install/windows
