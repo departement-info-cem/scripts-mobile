29ab6fb9b37e8f2134005096951c8d6791731cad
deguet <joris.deguet@cegepmontpetit.ca>
2024-02-28 11:49:13 -0500

    Premiere tentative

---
+++ sub-scripts/dotgradle-obtient.ps1
@@ -0,0 +1,8 @@
+﻿$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+. "$PSScriptRoot\urls-et-versions.ps1"
+. "$PSScriptRoot\fonctions.ps1"
+
+Start-Transcript -Path ${env:scripty.localTempPath}\transcript-dotgradle-obtient.txt
+Copy-Item  "${env:scripty.cachePath}\.gradle.7z" "${env:scripty.localTempPath}\.gradle.7z"
+powershell "${env:scripty.scriptPath}\dotgradle-installe.ps1"
+
