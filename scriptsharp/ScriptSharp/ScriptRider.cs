27ebf442cb78831a79808fbf755a8f961badf8ba
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-10-17 14:36:29 -0400

    Rider

--- scriptsharp/ScriptSharp/ScriptRider.cs
+++ scriptsharp/ScriptSharp/ScriptRider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -12,6 +13,13 @@ public static class ScriptRider
         await Utils.CopyFileFromNetworkShareAsync(
             Path.Combine(Config.LocalCache, "rider.7z"),
             Path.Combine(Config.LocalTemp, "rider.7z"));
+
+        await Utils.Unzip7ZFileAsync(
+            Path.Combine(Config.LocalTemp, "rider.7z"),
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                "rider")
+            );
 
         await UtilsRider.StartRider();
         LogSingleton.Get.LogAndWriteLine("     FAIT Installation de Rider complète");
