21f1e24833afeec5d3becf34cce1e9ba107a6942
deguet <joris.deguet@gmail.com>
2024-10-17 13:51:58 -0400

    Oups

--- scriptsharp/ScriptSharp/Utils/Utils.cs
+++ scriptsharp/ScriptSharp/Utils/Utils.cs
@@ -346,7 +346,6 @@ public static class Utils
         Environment.SetEnvironmentVariable(name, value);
         Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.User);
         Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.Process);
-        Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.Machine);
     }
 
     private static void DeleteThis(string path)
