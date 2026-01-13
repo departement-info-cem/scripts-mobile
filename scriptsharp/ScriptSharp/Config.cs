f35f69143845f1c3845ef6142c7493cdbd595f85
joris deguet <joris.deguet@gmail.com>
2026-01-13 10:01:21 -0500

    Version AS

--- scriptsharp/ScriptSharp/Config.cs
+++ scriptsharp/ScriptSharp/Config.cs
@@ -5,6 +5,7 @@ namespace ScriptSharp;
 
 public static class Config
 {
+    // TODO replace for 3M5 A26
     public const string Url3N5 = "https://github.com/departement-info-cem/3N5-Prog3/archive/refs/heads/main.zip";
     public const string Url4N6 = "https://github.com/departement-info-cem/4N6-Mobile/archive/refs/heads/master.zip";
     public const string Url5N6 = "https://github.com/departement-info-cem/5N6-mobile-2/archive/refs/heads/main.zip";
@@ -19,8 +20,8 @@ public static class Config
     public static readonly string LogPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "log");
     public static readonly string LogFilePath = Path.Combine(LogPath, "installation-log.txt");
 
-    public const string StudioUrl = "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2025.1.2.11/android-studio-2025.1.2.11-windows.zip";
-
+    public const string StudioUrl = "https://redirector.gvt1.com/edgedl/android/studio/install/2025.2.2.8/android-studio-2025.2.2.8-windows.exe";
+    
     // Flutter
     public const string FlutterSdk = "https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.24.0-stable.zip";
 }
